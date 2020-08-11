c9f983baad116904136c3b078a33ea70ff4f795e
sonang21 <sonang21@gmail.com>
2020-08-12 00:18:07 +0900

    Add Get Customers REST API Module

--- server.js
+++ server.js
@@ -10,4 +10,33 @@ app.get('/api/hello', (req, res) => {
     res.send({message: 'Hello Express!'});
 });
 
+app.get('/api/customers', (req, res) => {
+    res.send([
+         {
+            'id' : 1,
+            'image' : 'https://placeimg.com/64/64/1',
+            'name' : '홍길동',
+            'birthday' : '961222',
+            'gender' : '남자',
+            'job' : '대학생'
+          },
+          {
+            'id' : 2,
+            'image' : 'https://placeimg.com/64/64/2',
+            'name' : '이순신',
+            'birthday' : '991222',
+            'gender' : '남자',
+            'job' : '대학생'
+          },
+          {
+            'id' : 3,
+            'image' : 'https://placeimg.com/64/64/3',
+            'name' : '강감찬',
+            'birthday' : '941222',
+            'gender' : '여자',
+            'job' : '디자이너'
+          }
+    ])
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
