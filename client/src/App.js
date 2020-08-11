829522e87ef06abad2513ce2bbde04027cf9f3f7
sonang21 <sonang21@gmail.com>
2020-08-11 23:34:28 +0900

    Add Node.js Express Server Module

---
+++ client/src/App.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import Customer from './components/Customer'
+import './App.css';
+
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ( {
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.units * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : '홍길동',
+    'birthday' : '961222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '이순신',
+    'birthday' : '991222',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '강감찬',
+    'birthday' : '941222',
+    'gender' : '여자',
+    'job' : '대학생'
+  }
+]
+/****/
+class App extends Component {
+  render() {
+    const { classes } = this.props;
+      return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableHead>
+            <TableBody>
+              {
+                customers.map( 
+                  c => { 
+                          return( <Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} /> ); 
+                      }
+                )
+              }
+            </TableBody>
+          </Table>
+        </Paper>      
+      );
+  }
+}
+/****/
+/**
+function App() {
+    return (
+      <Paper className={styles.root}>
+        <Table className={styles.table}>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map( 
+                c => { 
+                        return( <Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} /> ); 
+                    }
+              )
+            }
+          </TableBody>
+        </Table>
+      </Paper>      
+    );
+}
+/**/
+export default withStyles(styles)(App);
--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component {
+    render() {
+        return (
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt='profile'/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+        )
+    }
+}
+
+class Customer2 extends React.Component {
     render() {
         return (
             <div>
