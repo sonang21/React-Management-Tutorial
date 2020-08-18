7ecdc48c409f0b4d2e858139594d0833e9ee8036
sonang21 <sonang21@gmail.com>
2020-08-18 16:27:04 +0900

    Apply App Bar and Hangul fonts

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme ({
+  typography: {
+    fontFamily: '"Noto Sans KR", serif',
+  }
+})
+
 ReactDOM.render(
-  <React.StrictMode>
+  <MuiThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
